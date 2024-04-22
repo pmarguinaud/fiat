e50a9a42f5f59b53fbad68ccf2c770017afa26da
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2024-04-22 09:25:45 +0000

    Generate MPL_SEND & MPL_RECV routines with DEVICE attribute on SEND/RECV buffers

---
+++ src/fiat/mpl/internal/mpl_recv_mod.intf.h
@@ -0,0 +1,19 @@
+
+INTERFACE MPL_RECV
+MODULE PROCEDURE &
+                 & SUBNAME (MPL_RECV_REAL4       )  , &
+                 & SUBNAME (MPL_RECV_REAL8       )  , &
+                 & SUBNAME (MPL_RECV_INT         )  , &
+                 & SUBNAME (MPL_RECV_REAL42      )  , &
+                 & SUBNAME (MPL_RECV_REAL43      )  , &
+                 & SUBNAME (MPL_RECV_REAL82      )  , &
+                 & SUBNAME (MPL_RECV_REAL83      )  , &
+                 & SUBNAME (MPL_RECV_INT_SCALAR  )  , &
+                 & SUBNAME (MPL_RECV_INT2        )  , &
+                 & SUBNAME (MPL_RECV_REAL4_SCALAR)  , &
+                 & SUBNAME (MPL_RECV_REAL8_SCALAR)  , &
+                 & SUBNAME (MPL_RECV_CHAR_SCALAR )  , &
+                 & SUBNAME (MPL_RECV_INT8        )  , &
+                 & SUBNAME (MPL_RECV_CHAR        )
+END INTERFACE
+
---
+++ src/fiat/mpl/internal/mpl_send_mod.intf.h
@@ -0,0 +1,19 @@
+
+INTERFACE MPL_SEND
+MODULE PROCEDURE &
+                 & SUBNAME (MPL_SEND_REAL4        )   , &
+                 & SUBNAME (MPL_SEND_REAL8        )   , &
+                 & SUBNAME (MPL_SEND_INT          )   , &
+                 & SUBNAME (MPL_SEND_REAL42       )   , &
+                 & SUBNAME (MPL_SEND_REAL43       )   , &
+                 & SUBNAME (MPL_SEND_REAL82       )   , &
+                 & SUBNAME (MPL_SEND_REAL83       )   , &
+                 & SUBNAME (MPL_SEND_INT_SCALAR   )   , &
+                 & SUBNAME (MPL_SEND_INT2         )   , &
+                 & SUBNAME (MPL_SEND_CHAR_SCALAR  )   , &
+                 & SUBNAME (MPL_SEND_REAL4_SCALAR )   , &
+                 & SUBNAME (MPL_SEND_REAL8_SCALAR )   , &
+                 & SUBNAME (MPL_SEND_INT8         )   , &
+                 & SUBNAME (MPL_SEND_CHAR         )
+END INTERFACE
+
